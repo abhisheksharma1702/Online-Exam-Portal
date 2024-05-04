0f2c6d735b9a867486af5be609c23c9748005cdb
Abhishek Sharma <60641490+abhisheksharma1702@users.noreply.github.com>
2024-05-04 22:52:37 +0530

    Delete ExamMicroservice/ExamMicroservice directory

--- ExamMicroservice/ExamMicroservice/ExamMicroservice/src/main/java/com/ExamMicroservice/ExamMicroservice/repositories/ProctorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ExamMicroservice.ExamMicroservice.repositories;
-import com.ExamMicroservice.ExamMicroservice.entities.Proctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProctorRepository extends JpaRepository<Proctor,Long> {
-}
