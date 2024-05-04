438fced955a50d6561608fe66c7e7f8b0fe8c8b0
Abhishek Sharma <60641490+abhisheksharma1702@users.noreply.github.com>
2024-05-04 22:53:44 +0530

    Delete userManager directory

--- userManager/userManager/userManager/src/main/java/com/userManager/userManager/repository/UserPasswordRepo.java
+++
@@ -1,9 +0,0 @@
-package com.userManager.userManager.repository;
-
-import com.userManager.userManager.entity.User;
-import org.springframework.data.repository.Repository;
-
-public interface UserPasswordRepo extends Repository<User,Integer>{
-
-		public User findByEmail(String email);
-}
